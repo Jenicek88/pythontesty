78bb9e65f9cbb6e16f7b572727da41ae16865bfd
jenicek <porubsky.java@gmail.com>
2020-03-30 22:40:40 +0200

    Prvni commit

---
+++ 01_print_promene.py
@@ -0,0 +1,4 @@
+
+strana = int(input('Zadej stranu čtverce v centimetrech: '))
+print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
+print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
